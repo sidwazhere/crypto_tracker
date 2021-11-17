1b137ec64235b1a512fb3d14309f893013e66926
Sideeq <sideeqw@gmail.com>
2021-11-17 18:21:00 -0500

    Initial Commit

---
+++ cry/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cry
+//
+//  Created by Sideeq on 11/17/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
